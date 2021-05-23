b06c1470383f8439feb684ae49434cdd74435f26
mekilic7 <mekilic7@gmail.com>
2021-05-23 21:15:55 +0300

    odev

--- odev.py
+++ odev.py
@@ -98,14 +98,8 @@ new_cols
 new_df = df[new_cols]
 [(F"NUM_{columns}").upper() for columns in df.columns if columns not in columns ]
 
-<<<<<<< HEAD
+
 new_df.head(5)
-=======
-
-
-
->>>>>>> origin/master
-
 
 import seaborn as sns
 df = sns.load_dataset("car_crashes")
